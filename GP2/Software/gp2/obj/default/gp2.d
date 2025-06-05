c41d5c9549f2c6f345501a1313ddce599771eb3f
ThomasYeh7414 <126331508+ThomasYeh7414@users.noreply.github.com>
2025-06-06 02:49:42 +1200

    software

---
+++ GP2/Software/gp2/obj/default/gp2.d
@@ -0,0 +1,13 @@
+obj/default/gp2.o: gp2.c ../gp2_bsp/system.h ../gp2_bsp/linker.h \
+ ../gp2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../gp2_bsp//HAL/inc/io.h ../gp2_bsp//HAL/inc/alt_types.h
+
+../gp2_bsp/system.h:
+
+../gp2_bsp/linker.h:
+
+../gp2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../gp2_bsp//HAL/inc/io.h:
+
+../gp2_bsp//HAL/inc/alt_types.h:
